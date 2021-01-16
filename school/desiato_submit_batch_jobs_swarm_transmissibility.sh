2a876e054307fb7465fed7075bd7d05ae588e75b
Jana <jana.lasser@ds.mpg.de>
2021-01-16 11:09:00 +0100

    reduced transmissibility range

--- school/desiato_submit_batch_jobs_swarm_transmissibility.sh
+++ school/desiato_submit_batch_jobs_swarm_transmissibility.sh
@@ -12,7 +12,7 @@ src=/home/jlasser/agent_based_COVID_SEIRX/data/school/representative_schools
 dst=/home/jlasser/agent_based_COVID_SEIRX/data/school/results_transmissibility
 
 
-for trisk in 1.1 1.2 1.3 1.4 1.5 1.6 1.7 1.8 1.9 2
+for trisk in 1.3 1.4
 	do
 	
 	for m_idx in $(seq 0 287)
