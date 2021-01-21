5c7a33f93a2812ffe7f80d3e68e30ca03c34f2c6
Jana <jana.lasser@ds.mpg.de>
2021-01-21 08:55:56 +0100

    more trisks

--- school/marvin_submit_batch_jobs_swarm_transmissibility.sh
+++ school/marvin_submit_batch_jobs_swarm_transmissibility.sh
@@ -5,33 +5,35 @@ echo -n "start: "
 date
 
 N_runs=500
-
-trisk=1.5           
+          
 max_tasks=32                 ## number of tasks per node.
 running_tasks=0              ## initialization
 src=/home/lasser/agent_based_covid/agent_based_COVID_SEIRX/data/school/representative_schools
 dst=/home/lasser/agent_based_covid/agent_based_COVID_SEIRX/data/school/results_transmissibility
 
 
-for stype in secondary primary_dc lower_secondary_dc secondary_dc
+for trisk in 1.3 1.7 1.9 
 	do
-	
-	for m_idx in $(seq 0 287)
+	for stype in secondary primary_dc lower_secondary_dc secondary_dc
 		do
-		running_tasks=`ps -C python --no-headers | wc -l`
 		
-		while [ "$running_tasks" -ge "$max_tasks" ]
+		for m_idx in $(seq 0 287)
 			do
-			sleep 5
 			running_tasks=`ps -C python --no-headers | wc -l`
-		done
+			
+			while [ "$running_tasks" -ge "$max_tasks" ]
+				do
+				sleep 5
+				running_tasks=`ps -C python --no-headers | wc -l`
+			done
 
-		echo "*********************"
-		echo run_data_creation_transmissibility.py $stype $N_runs $m_idx $trisk $src $dst
-		python run_data_creation_transmissibility.py $stype $N_runs $m_idx $trisk $src $dst &
-		echo "*********************"
-		sleep 1
-		
+			echo "*********************"
+			echo run_data_creation_transmissibility.py $stype $N_runs $m_idx $trisk $src $dst
+			python run_data_creation_transmissibility.py $stype $N_runs $m_idx $trisk $src $dst &
+			echo "*********************"
+			sleep 1
+			
+		done
 	done
 done
 wait
