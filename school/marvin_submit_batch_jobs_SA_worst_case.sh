9b8d0ded37f5d412d7a39fb3069c61841ccad167
Jana <jana.lasser@ds.mpg.de>
2021-01-29 16:42:00 +0100

    added scripts for mask efficiency data creation and worst case scenarios

---
+++ school/marvin_submit_batch_jobs_SA_worst_case.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+uptime
+echo -n "start: "
+date
+
+N_runs=500
+          
+max_tasks=32                 ## number of tasks per node.
+running_tasks=0              ## initialization
+src=/home/lasser/agent_based_covid/agent_based_COVID_SEIRX/data/school/representative_schools_reduced_sizes
+dst=/home/lasser/agent_based_covid/agent_based_COVID_SEIRX/data/school/results_sensitivity_analysis/worst_case
+
+
+for stype in primary primary_dc lower_secondary lower_secondary_dc upper_secondary secondary
+	do
+	
+	for m_idx in $(seq 0 27)
+		do
+		running_tasks=`ps -C python --no-headers | wc -l`
+		
+		while [ "$running_tasks" -ge "$max_tasks" ]
+			do
+			sleep 5
+			running_tasks=`ps -C python --no-headers | wc -l`
+		done
+
+		echo "*********************"
+		echo run_data_creation_worst_case.py $stype $N_runs $m_idx $src $dst
+		python run_data_creation_worst_case.py $stype $N_runs $m_idx $src $dst &
+		echo "*********************"
+		sleep 1
+		
+	done
+done
+
+wait
+
+echo -n "end: "
+date
